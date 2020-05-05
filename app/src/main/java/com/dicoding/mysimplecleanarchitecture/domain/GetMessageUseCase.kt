43a920f4239be756416ab4e3489c1785766a9e0a
Arif <faizin.arief@gmail.com>
2020-05-05 16:51:15 +0700

    Fix : simplify usecase

--- app/src/main/java/com/dicoding/mysimplecleanarchitecture/domain/GetMessageUseCase.kt
+++ app/src/main/java/com/dicoding/mysimplecleanarchitecture/domain/GetMessageUseCase.kt
@@ -3,8 +3,6 @@ package com.dicoding.mysimplecleanarchitecture.domain
 class GetMessageUseCase(private val messageRepository: IMessageRepository) :
     UseCase<MessageEntity, String>() {
 
-    override fun run(params: String): MessageEntity {
-        return messageRepository.getWelcomeMessage(params)
-    }
+    override fun run(params: String): MessageEntity = messageRepository.getWelcomeMessage(params)
 
 }
