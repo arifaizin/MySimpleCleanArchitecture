d3937a7a811522ed823238e8e157c6d78bf523d8
Arif <faizin.arief@gmail.com>
2020-05-13 13:17:16 +0700

    Fix : use case type

--- app/src/main/java/com/dicoding/mysimplecleanarchitecture/domain/UseCase.kt
+++ app/src/main/java/com/dicoding/mysimplecleanarchitecture/domain/UseCase.kt
@@ -2,9 +2,9 @@ package com.dicoding.mysimplecleanarchitecture.domain
 
 abstract class UseCase<out Type, in Params> where Type : Any {
 
-    abstract fun run(params: Params): Any
+    abstract fun run(params: Params): Type
 
-    operator fun invoke(params: Params, onResult: (Any) -> Unit = {}) {
+    operator fun invoke(params: Params, onResult: (Type) -> Unit = {}) {
         onResult(run(params))
     }
 
